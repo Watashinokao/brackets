a52cdce894c164ec8060ac5004db4d672e441187
Darya <watashi.no.kao24@gmail.com>
2023-10-22 17:44:46 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const strArr = bracketsConfig.map(arr => {
+    return `${arr[0]}${arr[1]}`;
+  })
+  let prevLength = str.length
+
+  while (str.length) {
+    strArr.forEach((item) => {
+      str = str.replaceAll(item, "");
+    });
+    if (str.length === prevLength) {
+      return false;
+    }
+    prevLength = str.length;
+  }
+
+  return true;
 }
+
+
